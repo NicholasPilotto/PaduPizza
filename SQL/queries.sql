5be31adaa1d222ef3961ec743c6312ecf02a2189
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-01-03 15:54:22 +0100

    Query per il calcolo dello stipendio di un titolare di pizzeria in base al fatturato mensile delle sue pizzerie

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,5 +1,4 @@
 --- 1) Query per l'inserimento di un ordine nel Database
-
 BEGIN;
 WITH 
 	_id AS (
@@ -17,3 +16,14 @@ UPDATE scontrino SET totale_lordo = (SELECT * FROM total_price((SELECT MAX(id) F
 UPDATE scontrino SET iva = (SELECT * FROM total_vat((SELECT MAX(id) FROM scontrino))) WHERE id = (SELECT MAX(id) FROM scontrino);
 
 COMMIT;
+
+--- 2) Query per il calcolo dello stipendio mensile di un titolare di N pizzerie. Esso varia in base al fatturato totale
+---	   delle pezzerie da esso gestite
+SELECT
+	CASE WHEN fatturato < 1000 THEN 1000
+		  WHEN fatturato > 1000 THEN 1000 + fatturato + (fatturato * 0.1)
+		  WHEN fatturato > 2000 THEN 1000 + fatturato + (fatturato * 0.15) 
+		  WHEN fatturato > 3000 THEN 1000 + fatturato + (fatturato * 0.17) 
+		  WHEN fatturato > 4000 THEN 1000 + fatturato + (fatturato * 0.2)
+	END
+FROM month_earning('GRDMRC80A01G224Z', 1)
