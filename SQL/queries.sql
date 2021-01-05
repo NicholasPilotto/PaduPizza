59f0f5dffe0356e30b3afa3c729290c7f7cd90e4
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-01-05 17:16:42 +0100

    Risoluzione piccolo bug query 3

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,46 +1,50 @@
 --- 1) Query per l'inserimento di un ordine nel Database
 BEGIN;
-WITH 
-	_id AS (
-		INSERT INTO ordine (ora, dipendente, pizzeria, cliente) VALUES (NOW(), ?, ?, ?) RETURNING id
-	),
-	_comp AS (
-		INSERT INTO composizione_ordine (ordine, pizza, formato_pizza, aggiunte, rimozioni, ripetizioni) VALUES 
-		((SELECT id FROM _id), ?, ?, ?, ?, ?),
-		((SELECT id FROM _id), ?, ?, ?, ?, ?)
-	)
+	WITH
+		_id AS (
+			INSERT INTO ordine (ora, dipendente, pizzeria, cliente) VALUES (NOW(), ?, ?, ?) RETURNING id
+		),
+		_comp AS (
+			INSERT INTO composizione_ordine (ordine, pizza, formato_pizza, aggiunte, rimozioni, ripetizioni) VALUES 
+			((SELECT id FROM _id), ?, ?, ?, ?, ?),
+			((SELECT id FROM _id), ?, ?, ?, ?, ?)
+		)
 
-INSERT INTO scontrino (id, data, tipo_pagamento, totale_lordo, iva) VALUES ((SELECT id FROM _id), NOW(), ?, 0,0);
+	INSERT INTO scontrino (id, data, tipo_pagamento, totale_lordo, iva) VALUES ((SELECT id FROM _id), NOW(), ?, 0, 0);
 
-UPDATE scontrino SET totale_lordo = (SELECT * FROM total_price((SELECT MAX(id) FROM scontrino))) WHERE id = (SELECT MAX(id) FROM scontrino);
-UPDATE scontrino SET iva = (SELECT * FROM total_vat((SELECT MAX(id) FROM scontrino))) WHERE id = (SELECT MAX(id) FROM scontrino);
-
+	UPDATE scontrino SET totale_lordo = (SELECT * FROM total_price((SELECT MAX(id) FROM scontrino))) WHERE id = (SELECT MAX(id) FROM scontrino);
+	UPDATE scontrino SET iva = (SELECT * FROM total_vat((SELECT MAX(id) FROM scontrino))) WHERE id = (SELECT MAX(id) FROM scontrino);
 COMMIT;
 
 --- 2) Query per il calcolo dello stipendio mensile di un titolare di N pizzerie. Esso varia in base al fatturato totale
 ---	   delle pezzerie da esso gestite
 SELECT
-	CASE WHEN fatturato < 1000 THEN 1000
-		  WHEN fatturato > 1000 AND fatturato < 2000 THEN 1000 + fatturato + (fatturato * 0.1)
-		  WHEN fatturato > 2000 AND fatturato < 3000 THEN 1000 + fatturato + (fatturato * 0.15) 
-		  WHEN fatturato > 3000 AND fatturato < 4000 THEN 1000 + fatturato + (fatturato * 0.17) 
-		  WHEN fatturato > 4000 THEN 1000 + fatturato + (fatturato * 0.2)
-	END
+	CASE WHEN fatturato = 0 THEN 0
+			 WHEN fatturato > 0 AND fatturato < 1000 THEN 1000
+		   WHEN fatturato > 1000 AND fatturato < 2000 THEN 1000 + fatturato + (fatturato * 0.1)
+		   WHEN fatturato > 2000 AND fatturato < 3000 THEN 1000 + fatturato + (fatturato * 0.15)
+		   WHEN fatturato > 3000 AND fatturato < 4000 THEN 1000 + fatturato + (fatturato * 0.17)
+		   WHEN fatturato > 4000 THEN 1000 + fatturato + (fatturato * 0.2)
+	END AS stipendio
 FROM month_earning(?, ?)
 
 --- 3) Query per il calcolo dello stipendio mensile di un dipendente
-SELECT 
-	IF impiego = 'Domiciliare_Macchina' THEN 
-		(stipendio * night_at_work(dipendente.cf, ?) + km(dipendente.cf, ?, ?) * 0.3
-	ELSE (stipendio * night_at_work(dipendente.cf))
+SELECT distinct 
+	CASE WHEN dipendente.impiego = 'Domiciliare_Macchina' THEN  (stipendio * night_at_work.nights + km.km * 0.3)
+		  WHEN dipendente.impiego <> 'Domiciliare_Macchina' THEN stipendio * night_at_work.nights
+	END
 FROM dipendente
 LEFT JOIN pizzeria
 ON pizzeria.id = dipendente.pizzeria
-LEFT JOIN km_percorsi
-ON km_percorsi.dipendente = dipendente.cf
+LEFT JOIN turno
+ON turno.dipendente = dipendente.cf
 LEFT JOIN stipendio_base
 ON stipendio_base.impiego = dipendente.impiego
-WHERE pizzeria.id = ?
+LEFT JOIN night_at_work(dipendente.cf, ?, 2021)
+ON dipendente.cf = night_at_work.dip
+LEFT JOIN km(dipendente.cf, ?, 2021)
+ON dipendente.cf = km.dip
+WHERE dipendente.cf = ?
 
 --- 4) Query che seleziona tutte le pizzerie i quali magazzini contengono una quantità di ingrediente inferiore a 20
 SELECT pizzeria.id, ingrediente.nome, stock.quantita
@@ -89,5 +93,5 @@ LEFT JOIN pizzeria
 ON pizzeria.id = ordine.pizzeria
 LEFT JOIN amministrazione
 ON amministrazione.id = pizzeria.id
-WHERE date_part('month', scontrino.data) = 1
+WHERE date_part('month', scontrino.data) = ?
 GROUP BY pizzeria.id
